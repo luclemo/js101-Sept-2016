710f02877d6e29ffb2c3a565779b661dfc3d39cb
Lucas Lemonnier <ll@ttwebco.com>
2016-11-23 19:43:15 -0500

    Create display messages function to update DOM.

--- display.js
+++ display.js
@@ -17,42 +17,29 @@ const data = {
     "role": "appUser",
     "name": "Steve",
     "text": "Just put some vinegar on it",
-    "avatarUrl": "https://www.gravatar.com/image.jpg",
+    "avatarUrl": "https://i2.wp.com/d22assd9svjrp9.cloudfront.net/public/style_images/obsidian/profile/default_large.png?ssl=1",
     "received": 1439220041.586
-  }],
+  },
+  {
+      "_id": "55c8c1498590aa1900b7b8b2",
+      "authorId": "c7f6e6d6c3a637261bd9438d",
+      "role": "appUser",
+      "name": "Alice",
+      "text": "I like my chips salty",
+      "avatarUrl": "https://cdn-images-1.medium.com/fit/c/100/100/1*OKgBaGWPGZdbLrzPvrW3nQ.jpeg",
+      "received": 1439220158.622
+    }],
   "next": "https://api.smooch.io/v1/appusers/c7f6e6d6c3a637261bd9656f/messages?after=1471995721"
 }
 
-let avatar = data.messages[0].avatarUrl;
-let author = data.messages[0].name;
-let text = data.messages[0].text;
 
-// express()
-// 	.use(express.static( __dirname + '/_dist'))
-// 	.use(bodyParser.json())
-// 	.post('/', function(req, res) {
-// 		console.log(req.body);
-// 		res.end();
-// 	})
-// 	.get('/', function(req, res) {
-// 		// res.render('display', {title : 'home'});
-// 		console.log(req.query);
-// 		console.log('hello!');
-// 	})
-// 	.listen(8000, function() {
-// 		console.log("running on 8000!")
-// 	});
-
-// superagent
-// 	.get('/')
-// 	.end(function(err, res) {
-// 		console.log(res.body);
-// 	});
-
-// superagent
-// 	.post('/')
-// 	.send(data)
-// 	.end(function(err, res) {
-// 		console.log(res.body);
-// 	});
+express()
+	.use(express.static( __dirname + '/_dist'))
+	.use(bodyParser.json())
+	.get('/', function(req, res) {
+		res.json(data);
+	})
+	.listen(8000, function() {
+		console.log("running on 8000!")
+	});
 
